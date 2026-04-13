a078d9c82879bf8cf52a4282c1101daa1a018524
st <st@A510PC3>
2026-04-13 16:58:41 +0300

    Add project files.

---
+++ Task01/logic.h
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+#define DEFAULT_SIZE 100
+
+int find_average(int array1[], int arrray2[], int array3[]);
+string find_the_best_class(int average1, int average2, int average3);
